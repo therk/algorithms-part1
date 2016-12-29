03cddef9ad08c4310530784e0c73a63f2124a26f
Therk <therk@me.com>
2016-12-28 19:10:09 -0500

    Added function and test for getData and completed initial implementation of union and connected functions in UF class.

--- src/main/java/UF.java
+++ src/main/java/UF.java
@@ -1,15 +1,22 @@
 public class UF {
-	int size;
+	int[] data;
 
 	UF(int size) {
-		this.size = size;
+		data = new int[size];
+		for (int i = 0; i < size; i++) {
+			data[i] = i;
+		}
+	}
+
+	public int[] getData() {
+		return data;
 	}
 
 	public void union(int p, int q) {
-
+		data[p] = data[q];
 	}
 
 	public boolean connected(int p, int q) {
-		return false;	
+		return(data[p] == data[q]);
 	}
 }
--- src/test/java/UFTest.java
+++ src/test/java/UFTest.java
@@ -5,8 +5,13 @@ import static org.junit.Assert.*;
  * @author therk, @date 12/28/16 4:35 PM
  */
 public class UFTest {
+		@Test public void testGetData() {
+        UF uf = new UF(2);
+        assertArrayEquals("Make sure we get correct data back for size 2 UF", uf.getData(), new int[]{0,1});
+    }
+
     @Test public void testSimpleUnionAndConnected() {
-        UF uf = new UF(1);
+        UF uf = new UF(2);
         uf.union(0, 1);
         assertTrue("Connected 1 and 2 should return 'true'", uf.connected(0, 1));
     }
